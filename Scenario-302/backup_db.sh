ca02ce13edb941fe1f744f6f571004c9532eb1e1
DennyZhang <contact@dennyzhang.com>
2018-01-06 10:03:50 -0600

    db backup add script

--- Scenario-302/backup_db.sh
+++ Scenario-302/backup_db.sh
@@ -9,11 +9,23 @@
 ## Description :
 ## --
 ## Created : <2018-01-06>
-## Updated: Time-stamp: <2018-01-06 09:52:19>
+## Updated: Time-stamp: <2018-01-06 10:03:33>
 ##-------------------------------------------------------------------
 set -ex
 
-/usr/bin/mysqldump -ubn_wordpress -p8fwWcgBn5V \
-                   -P3306 -hmy-wordpress-mariadb \
-                   bitnami_wordpress --result-file=/backup/db_backup.sql
+date_timestamp=$(date +['%Y-%m-%d_%H%M%S'])
+db_username="bn_wordpress"
+db_password="8fwWcgBn5V"
+db_host="my-wordpress-mariadb"
+db_name="bitnami_wordpress"
+db_port="3306"
+
+backup_folder="/backup"
+backup_sql_file="db_backup_${date_timestamp}.sql"
+
+echo "mysqldump $db_name"
+/usr/bin/mysqldump \
+    -u${db_username} -p${db_password} \
+    -P${db_port} -h"${db_host}" "${db_name}" \
+    --result-file="${backup_folder}/${backup_sql_file}"
 ## File: hello ends
